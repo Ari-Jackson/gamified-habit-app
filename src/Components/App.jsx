4151530484f562e82c1dc3d4badbfdc17edd222a
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-29 15:47:11 -0500

    Moves App.jsx into Compnents

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,6 +5,9 @@ function App() {
 
   return (
     <div className="App">
+      <div className='border-black rounded-sm border-solid border-2'>
+        <h1 className='text-orange-700'>Scoreboard</h1>
+      </div>
     </div>
   )
 }
