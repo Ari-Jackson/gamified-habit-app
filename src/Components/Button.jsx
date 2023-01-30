470f23ca09053923fff7bcfaa4f32a8af0d47a65
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-29 22:52:33 -0500

    Creates Button Element

---
+++ src/Components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({ children }) {
+  return (
+    <button class="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+      {children}
+    </button>
+  );
+}
