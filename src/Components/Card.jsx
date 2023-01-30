cf20ed9dbd834cb8e131da09ffd76f073340d9a6
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-30 15:42:54 -0500

    Corrects className property and updates styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ children }) {
   return (
-    <div class="mx-auto flex space-y-0 space-x-6 rounded-xl bg-white px-8 py-4 shadow-lg">
+    <div className="mx-auto mb-3 flex w-80 space-y-0 space-x-6 rounded-xl bg-white shadow-lg">
       {children}
     </div>
   );
