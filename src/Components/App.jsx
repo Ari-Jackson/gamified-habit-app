0550f550607954fb5222b3d9934c077f7194e5a1
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-30 21:11:46 -0500

    Adds Habit and Todo Components to App

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-import { Todo } from "./Todos";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Habit from "./Habit";
+import Todo from "./Todos";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App h-screen bg-teal-500">
-      <Todo />
+      <h1 className=" pb-5 text-center text-slate-200">Habit App</h1>
+      <div className="m-auto mb-2 w-fit">
+        <FontAwesomeIcon className="text-3xl text-white" icon={faPlus} />
+      </div>
+      <Habit todoTitle={"Wash Dishes"} emoji={"🍽"} total={1} />
+      <Habit todoTitle={"Drink Water"} emoji={"💦"} total={8} units={"cups"} />
+      <Habit todoTitle={"Meditate"} emoji={"🧘🏽‍♀️"} total={25} units={"minutes"} />
+      <Todo todoTitle={"Meditate @ Home"} emoji={"🧘🏽‍♀️"} />
     </div>
   );
 }
