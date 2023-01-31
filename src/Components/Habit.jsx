84865069c1f573e0b8e32236e8b174ac41086171
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-30 21:10:45 -0500

    Adjusts width of component div

--- src/Components/Habit.jsx
+++ src/Components/Habit.jsx
@@ -20,16 +20,16 @@ export default function Habit({ todoTitle, emoji, total, units }) {
     <Card onClick={() => handleClick()}>
       <div
         style={fillerStyles}
-          className="flex h-full w-full space-y-0 space-x-6 rounded-lg bg-violet-400 py-4 text-right transition-[width] duration-500 ease-in-out"
-        >
-          <span className="ml-8 block self-center rounded-lg bg-slate-100 p-3 text-4xl">
-            {emoji}
-          </span>
-          <div className="space-y-2 text-left">
-            <div className="space-y-0.5">
-              <p className="w-max text-lg font-semibold text-black">
-                {todoTitle}
-              </p>
+        className="flex h-full space-y-0 space-x-6 rounded-lg bg-violet-400 py-4 text-right transition-[width] duration-500 ease-in-out"
+      >
+        <span className="ml-8 block self-center rounded-lg bg-slate-100 p-3 text-4xl">
+          {emoji}
+        </span>
+        <div className="space-y-2 text-left">
+          <div className="space-y-0.5">
+            <p className="w-max text-lg font-semibold text-black">
+              {todoTitle}
+            </p>
             <p className="w-max font-semibold text-slate-300">
               {count} / {total} {units}
             </p>
