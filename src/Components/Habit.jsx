6e293b304940028fc03d041555c6c8968705436b
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-01 17:12:40 -0500

    Updates styles for components that use Card

--- src/Components/Habit.jsx
+++ src/Components/Habit.jsx
@@ -17,28 +17,30 @@ export default function Habit({ todoTitle, emoji, total, units }) {
   };
 
   return (
-    <Card onClick={() => handleClick()}>
-      <div
-        style={fillerStyles}
-        className="flex h-full space-y-0 space-x-6 rounded-lg bg-violet-400 py-4 text-right transition-[width] duration-500 ease-in-out"
-      >
-        <span className="ml-8 block self-center rounded-lg bg-slate-100 p-3 text-4xl">
-          {emoji}
-        </span>
-        <div className="space-y-2 text-left">
-          <div className="space-y-0.5">
-            <p className="w-max text-lg font-semibold text-black">
-              {todoTitle}
-            </p>
-            <p className="w-max font-semibold text-slate-300">
-              {count} / {total} {units}
-            </p>
+    <div className="m-auto w-[25rem] cursor-pointer">
+      <Card onClick={() => handleClick()}>
+        <div
+          style={fillerStyles}
+          className="flex h-full space-y-0 space-x-6 rounded-lg bg-violet-400 py-4 text-right transition-[width] duration-500 ease-in-out"
+        >
+          <span className="ml-8 block self-center rounded-lg bg-slate-100 p-3 text-4xl">
+            {emoji}
+          </span>
+          <div className="space-y-2 text-left">
+            <div className="space-y-0.5">
+              <p className="w-max text-lg font-semibold text-black">
+                {todoTitle}
+              </p>
+              <p className="w-max font-semibold text-slate-300">
+                {count} / {total} {units}
+              </p>
+            </div>
           </div>
+          <span className="block self-center text-2xl text-slate-700 opacity-80">
+            <FontAwesomeIcon icon={faEllipsis} />
+          </span>
         </div>
-        <span className="block self-center text-2xl text-slate-700 opacity-80">
-          <FontAwesomeIcon icon={faEllipsis} />
-        </span>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 }
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -15,26 +15,28 @@ export default function Todo({ todoTitle, emoji }) {
   };
 
   return (
-    <Card onClick={handleClick}>
-      <div
-        style={fillerStyles}
-        className="flex h-full space-y-0 space-x-10 rounded-lg bg-violet-400 py-4 text-right transition-[width] duration-500 ease-in-out"
-      >
-        <span className="ml-8 block self-center rounded-lg bg-slate-100 p-3 text-4xl">
-          {emoji}
-        </span>
-        <div className="space-y-2 text-left">
-          <div className="space-y-0.5">
-            <p className="w-max text-lg font-semibold text-black">
-              {todoTitle}
-            </p>
-            <p className="font-semibold text-slate-300">Date</p>
+    <div className="mx-auto w-[25rem] cursor-pointer">
+      <Card onClick={handleClick}>
+        <div
+          style={fillerStyles}
+          className="flex h-full space-y-0 space-x-10 rounded-lg bg-violet-400 py-4 text-right transition-[width] duration-500 ease-in-out"
+        >
+          <span className="ml-8 block self-center rounded-lg bg-slate-100 p-3 text-4xl">
+            {emoji}
+          </span>
+          <div className="space-y-2 text-left">
+            <div className="space-y-0.5">
+              <p className="w-max text-lg font-semibold text-black">
+                {todoTitle}
+              </p>
+              <p className="font-semibold text-slate-300">Date</p>
+            </div>
           </div>
+          <span className="block self-center text-2xl text-slate-700">
+            <FontAwesomeIcon icon={faEllipsis} />
+          </span>
         </div>
-        <span className="block self-center text-2xl text-slate-700">
-          <FontAwesomeIcon icon={faEllipsis} />
-        </span>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 }
