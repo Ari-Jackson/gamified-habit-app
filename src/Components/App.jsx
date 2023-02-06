65616264144115cf4f16d697df0b9ccf95f5b4b9
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-06 18:03:44 -0500

    Adjusts app to show main page setup

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,35 +1,17 @@
-import Habit from "./Habit";
-import Todo from "./Todos";
 import SideBar from "./SideBar";
-import StatsCard from "./StatsCard";
+import MainStatsCard from "./MainStatsCard";
 
-function App() {
+export default function App() {
   return (
-    <div className="App ">
-      <div className="fixed h-screen w-screen bg-hero-background bg-cover bg-no-repeat">
+    <div className="App">
+      <div className="fixed h-full w-full bg-hero-background bg-cover bg-no-repeat">
         <SideBar />
         <img
           src="src/assets/images/f5a885b5d781e05edde29c4eb8ec48ee61904b62.png"
           className="fixed right-24 h-screen"
         />
-        {/* <Habit todoTitle={"Wash Dishes"} emoji={"🍽"} total={1} />
-        <Habit
-          todoTitle={"Drink Water"}
-          emoji={"💦"}
-          total={8}
-          units={"cups"}
-        />
-        <Habit
-          todoTitle={"Meditate"}
-          emoji={"🧘🏽‍♀️"}
-          total={25}
-          units={"minutes"}
-        />
-        <Todo todoTitle={"Meditate @ Home"} emoji={"🧘🏽‍♀️"} /> */}
-        <StatsCard />
+        <MainStatsCard />
       </div>
     </div>
   );
 }
-
-export default App;
