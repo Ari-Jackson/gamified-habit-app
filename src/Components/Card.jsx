ea4f725d05d0d99ee707d72d5647382e44816df6
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-05 22:50:41 -0500

    Removes shadow from Card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ children, onClick }) {
   return (
     <div onClick={onClick} className="h-full w-full">
-      <div className="mx-auto mb-3 flex h-full max-w-7xl space-y-0 space-x-6 rounded-xl bg-white shadow-lg">
+      <div className="mx-auto mb-3 flex h-full max-w-7xl space-y-0 space-x-6 rounded-xl bg-white">
         {children}
       </div>
     </div>
