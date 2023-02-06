ee06b9519c52cd427bdfc380cfc9df1e2cf8a28f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-06 18:02:10 -0500

    Adds MainStatsCard Component

---
+++ src/Components/MainStatsCard.jsx
@@ -0,0 +1,71 @@
+import DailySummary from "./DailySummary";
+import StatusBox from "./StatusBox";
+import { faCoins, faGem, faHeart } from "@fortawesome/free-solid-svg-icons";
+
+export default function MainStatsCard() {
+  return (
+    <div className="ml-40 mt-20 h-auto w-[650px] max-w-7xl space-y-0 space-x-6 rounded-xl bg-white pb-3">
+      <div className="flex h-auto w-full flex-col">
+        {/* ------------------------------------ Character Info ------------------------------------*/}
+        <div className="flex-box-center space-x-6 p-5 font-tech">
+          <p className="rounded-full bg-blue-500 p-4 text-center">House Logo</p>
+          <div className="mt-4 w-full">
+            <h1 className="text-left font-bold text-slate-700">Ari-the-don</h1>
+            <h3 className="text-2xl text-slate-600">Level 1</h3>
+          </div>
+        </div>
+        <hr />
+        {/* ------------------------------------ Status Bars ------------------------------------*/}
+
+        <div className="mx-6 my-2 flex h-fit flex-col rounded-md p-5 font-tech text-lg hover:cursor-pointer hover:bg-slate-200">
+          <div className="mx-1 flex items-center">
+            <p className="flex-grow text-2xl">XP</p>
+            <p>1500</p>
+          </div>
+          <ProgressBar completed={(750 / 1500) * 100} />
+        </div>
+        <div className="mx-6 my-2 flex h-24 justify-around space-x-5">
+          {statBoxInfo.map(({ title, number, icon, color }) => (
+            <StatusBox
+              title={title}
+              number={number}
+              icon={icon}
+              color={color}
+            />
+          ))}
+        </div>
+        <DailySummary />
+      </div>
+    </div>
+  );
+}
+
+{
+  /* ------------------------------------ Status Box Info ------------------------------------*/
+}
+
+let statBoxInfo = [
+  { title: "Gold", number: 365, icon: faCoins, color: "text-amber-500" },
+  { title: "Mana", number: 10, icon: faGem, color: "text-cyan-500" },
+  { title: "Heart", number: 12, icon: faHeart, color: "text-rose-500" },
+];
+
+{
+  /* ------------------------------------ Progress Bars ------------------------------------*/
+}
+
+const ProgressBar = ({ completed }) => {
+  const fillerStyles = {
+    width: `${completed}%`,
+  };
+
+  return (
+    <div className="h-4 w-full rounded-full bg-gray-300">
+      <div
+        className={"h-full rounded-full bg-yellow-200 text-right"}
+        style={fillerStyles}
+      ></div>
+      <span></span>
+    </div>
+  );
+};
