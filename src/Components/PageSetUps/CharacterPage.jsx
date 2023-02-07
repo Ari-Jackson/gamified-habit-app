7ffe2c5b22ad9898398035672a8ba8b37a2da3f6
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-07 10:03:14 -0500

    Moves Character Page setup into its own page

---
+++ src/Components/PageSetUps/CharacterPage.jsx
@@ -0,0 +1,17 @@
+import SideBar from "../SideBar";
+import MainStatsCard from "../MainStatsCard";
+
+export default function CharacterPage() {
+  return (
+    <main className="App">
+      <div className="fixed h-full w-full select-none bg-hero-background bg-cover bg-no-repeat">
+        <SideBar />
+        <img
+          src="src/assets/images/f5a885b5d781e05edde29c4eb8ec48ee61904b62.png"
+          className="fixed right-24 h-screen"
+        />
+        <MainStatsCard />
+      </div>
+    </main>
+  );
+}
