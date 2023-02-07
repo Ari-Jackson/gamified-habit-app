e8e0e1b15f5ae49f2d69b2fb1aa607885fbcc738
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-07 10:32:30 -0500

    Adds background assets to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-background": "url('/src/assets/images/desktop_background-1.jpg')",
+        "market-background": "url(src/assets/images/desktop_background.jpg)",
+        "lake-background": "url('/src/assets/images/desktop_background-1.jpg')",
+        "inn-background": "url(src/assets/images/desktop_background-2.jpg)",
+        "hero-background": "url(src/assets/images/desktop_background-3.jpg)",
+        "cliff-background": "url(/src/assets/images/desktop_background-4.jpg)",
+        "mask-hero-character":
+          "url('/src/assets/images/11331b5cf7f853a4717cf8df4ccad807e47c1b09.png')",
         "hero-character":
           "url('/src/assets/images/f5a885b5d781e05edde29c4eb8ec48ee61904b62.png')",
       },
