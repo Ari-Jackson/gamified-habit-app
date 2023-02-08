7ecfa5b403b3ec2b718dea146c9383913c6297c2
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-08 09:53:05 -0500

    Refractors styling

--- src/Components/CharacterInfo.jsx
+++ src/Components/CharacterInfo.jsx
@@ -1,10 +1,10 @@
 export default function CharcterInfo() {
   return (
-    <div className="flex-box-center space-x-6 p-5 font-tech">
-      <p className="rounded-full bg-blue-500 p-4 text-center">House Logo</p>
-      <div className="mt-4 w-full">
+    <div className="flex-box-center space-x-6 font-tech">
+      <p className="rounded-full bg-blue-500 p-2 text-center">House Logo</p>
+      <div className="flex-grow">
         <h1 className="text-left font-bold text-slate-700">Ari-the-don</h1>
-        <h3 className="text-2xl text-slate-600">Level 1</h3>
+        <h2 className="text-slate-600">Level 1</h2>
       </div>
     </div>
   );
